558f72fe8b5116ec0d3b875e9cc671e21bac6782
Andrew Marshall <alm266@cd.cs.rutgers.edu>
2016-11-10 20:59:39 -0500

    fixed copress bugs

--- compress.c
+++ compress.c
@@ -4,18 +4,21 @@
 
 int main(int argc, char *argv[])
 {
-	//printf("Enter your string. Repeated tmpters will be \"compressed\"");
 	int len = strlen(argv[1]);
 	printf("LEN: %d\n", len);
 	
+	if(len < 3){
+		printf("%s\n", argv[1]);
+		return;
+	}
+	
 	char input[len];
 	strcpy(input, argv[1]);
+	
 	char output[len];
 	char arrtmp[len];
-	//output[len-1] = '\0';
 	
 	char tmp;
-	//char tmp2;
 	int num = 1;
 	
 	int i;
@@ -26,17 +29,13 @@ int main(int argc, char *argv[])
 			tmp = input[i];
 			num=3;
 	
-			for(i=i+2;i< len-1;i++)
+			for(i=i+2;i< len;i++)
 			{
 				
 				if(input[i] == input[i+1])
 					num++;
 				else
 				{
-					//char tmp2[20];
-					//itoa(tmp2, num, 10);
-					//strcat(output, tmp2);
-					//strcat(output, tmp);
 					memset(&arrtmp[0], 0, len);
 					snprintf(arrtmp, len, "%d%c", num, tmp);
 					strcat(output, arrtmp);
@@ -48,8 +47,6 @@ int main(int argc, char *argv[])
 		else
 		{
 			tmp = input[i];
-			//output = output + tmp;
-			//strcat(output, tmp);
 			memset(&arrtmp[0], 0, len);
 			snprintf(arrtmp, len, "%c", tmp);
 			strcat(output, arrtmp);
@@ -57,13 +54,9 @@ int main(int argc, char *argv[])
 		}
 		printf("%s\n", output);
 	}
-	//output = output + num + tmp;
-	//itoa(tmp2, num, 10);
-	//strcat(output, tmp2);
-	tmp = input[i];
+	/*tmp = input[i];
 	memset(&arrtmp[0], 0, len);
 	snprintf(arrtmp, len, "%d", tmp);
-	strcat(output, arrtmp);
-	//strcat(output, tmp);
-	printf("%s\n", output);
+	strcat(output, arrtmp);*/
+	//printf("%s\n", output);
 }
