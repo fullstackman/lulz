17fdd581c06b03f9f04aa8f9aca2aadd9d2cfb19
Andrew Marshall <alm266@python.cs.rutgers.edu>
2016-11-18 21:41:45 -0500

    Added file output, fixed processes and compression

--- compressR_LOLS.c
+++ compressR_LOLS.c
@@ -4,6 +4,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 int main(int argc, char *argv[])
 {
@@ -11,16 +14,87 @@ int main(int argc, char *argv[])
 	FILE * fp;
 	fp = fopen(argv[1], "r");
 	
+	if(fp == NULL){
+		printf("That file was not found!\n");
+		return 404;
+	}
+	
+	int fileNameLength = strlen(argv[1])-4;
+	char fileName[fileNameLength+1];
+	strncpy(fileName, argv[1], fileNameLength);
+	fileName[fileNameLength+1] = '\0';
+	printf("Here is the file name: %s\n",fileName);
+	
 	//get size of text in file
-	size_t pos = ftell(fp);
-	fseek(f, 0, SEEK_END);
-	size_t fsize = ftell(fp);
-	fseek(f, pos, SEEK_SET);
+	//size_t pos = ftell(fp);
+	fseek(fp, 0, SEEK_END);
+	//ftell was giving us one too many!
+	size_t fsize = ftell(fp) - 1;
+	//fseek(fp, pos, SEEK_SET);
+	
+	fclose(fp);
+	int numParts = atoi(argv[2]);
+	
+	//is user asking for more parts than there are characters?
+	if(numParts > fsize){
+		printf("ERROR: Too many parts requested\n");
+		return -1;
+	}
 	
 	//find size of parts
-	int parts = atoi(argv[2]);
-	int sizepart = fsize/parts;	//remember to account for remainder
+	int sizePart = fsize/numParts;
+	int offset = fsize % numParts;
+	
+	printf("This is how many characters we can squeeze into each file: %d\n", sizePart);
 	
 	//here we should store the indeces to start and how much to do for each process
 	//fork() "parts" number of parts
+	pid_t pids[numParts];
+	int i;
+	for(i=0; i<numParts; i++)
+	{
+		//first part size is largest
+		//includes any leftover characters
+		pids[i] = fork();
+		
+		if(pids[i] == 0){
+			printf("PROCESS %d RUNNING\n", i);
+			
+			char sp[32];
+			sprintf(sp, "%d", sizePart);
+			
+			char is[32];
+			sprintf(is, "%d", i);
+			
+			
+			
+			if(i == 0){		//if its the first part, increment by offset
+				
+				char command[510];
+				snprintf(command, 510, "./compressR_worker_LOLS %s %d %d %d", argv[1], (sizePart + offset), i, 0);
+				execl("/bin/sh", "/bin/sh", "-c", command, NULL);
+				//continue; 	<---not necessary
+			}
+			
+			char command[510];
+			snprintf(command, 510, "./compressR_worker_LOLS %s %d %d %d", argv[1], sizePart, i, ( (i * sizePart) + offset) );
+			
+			//char* command = "./compressR_worker_LOLS \"test.txt\" 2";
+			execl("/bin/sh", "/bin/sh", "-c", command, NULL);
+		}
+		else{
+			continue;
+		}
+	}
+	
+	//now we need to wait for each child process to finish working
+	for(i=0; i<numParts; i++){
+		int status;
+		while ((pids[i] = wait(&status)) > 0)
+		{
+			printf("Exit status of %d was %d (%s)\n", (int)pids[i], status,
+				   (status > 0) ? "reject" : "accept");
+		}
+	}
+	return EXIT_SUCCESS;
 }
--- compressR_worker_LOLS.c
+++ compressR_worker_LOLS.c
@@ -1,1 +1,89 @@
-//Needs more code
+//child process code to be exec'd
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+	char* fileName = argv[1];
+	int sizePart = atoi(argv[2]);
+	int orderNum = atoi(argv[3]);
+	int totalOffset = atoi(argv[4]);
+	
+	FILE *fp;
+	fp = fopen(fileName, "r");
+	fseek(fp, totalOffset, SEEK_SET);
+	
+	char input[sizePart];
+	//fgets wasn't inclusive of the final character so we add one
+	fgets(input, sizePart+1, fp);
+	
+	char output[sizePart];
+	output[0] = '\0';
+	if(sizePart > 2){
+		char arrtmp[2];
+		int num = 1;
+		int i;
+		
+		for(i=1; i<sizePart; ++i){
+			if(input[i] != input[i-1]){
+				switch(num){
+					case 1:
+						//print previous letter
+						strncat(output, (char*) &input[i-1], 1);
+						break;
+					case 2:
+						//print twice
+						strncat(output, (char*) &input[i-1], 1);
+						strncat(output, (char*) &input[i-1], 1);
+						break;
+					default:
+						//print num times
+						sprintf(arrtmp, "%d%c", num, input[i-1]);
+						strcat(output, arrtmp);
+				}
+				num = 1;
+			}
+			else
+				++num;
+		}
+		switch(num){
+			case 1:
+				//print previous letter
+				strncat(output, (char*) &input[i-1], 1);
+				break;
+			case 2:
+				//print twice
+				strncat(output, (char*) &input[i-1], 1);
+				strncat(output, (char*) &input[i-1], 1);
+				break;
+			default:
+				//print num times
+				sprintf(arrtmp, "%d%c", num, input[i-1]);
+				strcat(output, arrtmp);
+		}
+	}
+	else
+		strcpy(output, input);
+	printf("\tWorker Output - PART %d: %s\n", orderNum, output);
+	
+	FILE* sampleOutput;
+	//int fileNameLength = strlen(argv[1])-4;
+	//char outputFile[fileNameLength+11];
+	char outputFile[510];
+	fileName[sizeof(fileName)- 4] = '_';
+	snprintf(outputFile, 510, "%s_LOLS%d", fileName, orderNum);
+	//strcpy(outputFile, fileName);
+	//strcat(outputFile, "OUTPUT.txt");
+	//outputFile[fileNameLength+10] = '\0';
+	sampleOutput = fopen(outputFile, "w+");
+	
+	fputs(output,sampleOutput);
+	fclose(fp);
+	
+	//success status is returned
+	exit(0);
+	//when would this process fail though?
+	printf("\tWorker %d failed.\n", orderNum);
+	return 0;
+}
